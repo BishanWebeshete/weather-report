b650e7a29f04d522a8337d74ac7c836b9254a544
Bishan Webeshete <webesheteb@gmail.com>
2023-11-13 16:37:14 -0800

    updated types and home page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,67 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { optionType } from "./types/index";
 import "./App.css";
 
 function App() {
-  const [term, setTerm] = useState("");
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    setTerm(e.target.value);
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+  const [city, setCity] = useState<optionType | null>(null);
+
+  const getSearchOptions = async (value: string) => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()},&limit=5&appid=${
+          process.env.REACT_APP_API_KEY
+        }`
+      );
+      if (!response.ok) {
+        throw new Error(`Bad Server Repsonse, ${response.status}`);
+      }
+      const jsonData = await response.json();
+      setOptions(jsonData);
+      console.log(jsonData);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}
-  //&appid={API key}
+  const getForecast = async (city: optionType) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error(`Bad Server Response ${response.status}`);
+      }
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const onSubmit = () => {
+    if (!city) return;
+    getForecast(city);
+  };
+
+  const onOptionSelect = async (option: optionType) => {
+    setCity(option);
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+    if (value === "") return;
+    getSearchOptions(value);
+  };
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
@@ -23,16 +75,30 @@ function App() {
         <p className="text-sm mt-2">
           Please Enter a location, and choose an option from the dropdown
         </p>
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
             value={term}
             className="px-2 py-1 rounded-l-md border-2 border-white"
             onChange={onInputChange}
           />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={`${option.name}-${index}`}>
+                <button
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white
+                px-2 py-1 cursor-pointer"
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
           <button
             className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500
         text-zinc-100 px-2 py-1 cursor-pointer"
+            onClick={onSubmit}
           >
             Search
           </button>
